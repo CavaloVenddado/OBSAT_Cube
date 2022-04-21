148045bcd2283a584e6fc12e1ce8c629de52f228
felipetrentin <felipetrentintecnico@hotmail.com>
2022-04-21 18:17:41 -0300

    incorporate new features to main code.

--- src/main.cpp
+++ src/main.cpp
@@ -3,77 +3,66 @@
 #include "FS.h"                // SD Card ESP32
 #include "SD.h"            // SD Card ESP32
 #include "cameraCompanion.h"
-
-#define I2C_DEV_ADDR 0x30
-#define PIN_SD_SS 15
+#include "logger.h"
 
 camCompanion myESPcamera(Serial2);
 System cubesat;
-
+Logs myLogger;
 void setup() {
-    cubesat.init();
+    cubesat.initNoNetwork();
     //Serial.begin(115200);
     Serial2.begin(115200);
+    myLogger.init();
+    myLogger.createFileStructure();
     while(Serial2.available()){
         Serial2.read();
     }
     Serial2.flush();
     Serial.setDebugOutput(true);
-    if (cubesat.getSDStatus() == NO_SD){
-        cubesat.toggleSD();
-    }
-    while(cubesat.getSDStatus() == NO_SD){
-        Serial.println("Impossível continuar, nenhum cartão sd!");
-        delay(5000);
-    }
-    #ifndef PION_SYSTEM_H
-    pinMode(PIN_SD_SS, OUTPUT);
-    if(!SD.begin(PIN_SD_SS)){
-        Serial.println("SD Card Mount Failed");
-    }
-    uint8_t cardType = SD.cardType();
-    if(cardType == CARD_NONE){
-        Serial.println("No SD Card attached");
-        return;
-    }
-    /*
-    interface.doBeeps(3, 20, 100);
-    interface.toggleSiren();
-    interface.setLed(0, false);
-    */
-    #endif
     //cubesat.doBeeps(3, 100, 500);
     //cubesat.activateSDLog();
     delay(10000);
-    
+    myLogger.log("Sistema iniciado", INFO);
 }
 int imgnum = 0;
+
+/**
+ * @brief function for taking pictures with espcamera
+ * 
+ * @param path path where image will be stored (has to include filename and extension)
+ * @param fs system's filesystem
+ */
 void picture(String path, fs::FS &fs){
-    
     Serial.printf("Picture file name: %s\n", path.c_str());
-    
+    //open the file
     File file = fs.open(path.c_str(), FILE_WRITE);
     if(!file){
         Serial.println("Failed to open file in writing mode");
+        return;
     }
-    myESPcamera.requestFrame();
+    myESPcamera.requestFrame();//take picture
     Serial.println("recv");
-    myESPcamera.receiveFrame();
+    //log if the frame was not fully received
+    if(!myESPcamera.receiveFrame()){
+        myLogger.log("Frame ["+ path +"] was received partially", WARN);
+    }
     Serial.println("writing");
     file.write(myESPcamera.imgBuf, myESPcamera.imgBufLen);
     file.close();
     myESPcamera.freeFrame();
     Serial.println("done");
 }
+
 void loop()
 {
     //digitalWrite(LAMP_PIN, LOW); 
-    String name = "/picture" + String(imgnum) + ".jpg";
+    String name = myLogger.extrasDirPath + String(imgnum) + ".jpg";
     cubesat.setRGB(PURPLE);
     picture(name, SD);
     cubesat.setRGB(OFF);
     imgnum ++;
-    cubesat.setLed(1, HIGH);
-    delay(10000);
-    cubesat.setLed(1, LOW);
+    cubesat.setLed(L1, HIGH);
+    myLogger.log("imagem " + name + " capturada", DATA);
+    myLogger.datalog();
+    cubesat.setLed(L1, LOW);
 }
